8037d209ea367050aaecf847b336ebc144444093
Me <rafaleto@gmail.com>
2022-08-26 20:28:43 +0200

    feat: First methods for json processing

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,25 @@
   windows_subsystem = "windows"
 )]
 
+// Load all json content from a path
+fn load_json(path: &str) -> Result<serde_json::Value, Box<dyn std::error::Error>> {
+  let mut file = File::open(path)?;
+  let mut contents = String::new();
+  file.read_to_string(&mut contents)?;
+  Ok(serde_json::from_str(&contents)?)
+}
+
+// Get unique keys from a json object
+fn unique_keys_from_json(json: &serde_json::Value) -> Vec<String> {
+  let mut keys = Vec::new();
+  for (key, _) in json.as_object().unwrap() {
+    if !keys.contains(&key.to_string()) {
+      keys.push(key.to_string());
+    }
+  }
+  keys
+}
+
 fn main() {
   tauri::Builder::default()
     .run(tauri::generate_context!())
